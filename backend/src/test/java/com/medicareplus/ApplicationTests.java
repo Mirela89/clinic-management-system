9d0ed0fe97081aa786e886e177a1c42eaaaee6f9
Mirela89 <ddrt91@hotmail.com>
2026-03-26 22:15:28 +0200

    configurare multi-environment si docker-compose

--- backend/src/test/java/com/medicareplus/ApplicationTests.java
+++ backend/src/test/java/com/medicareplus/ApplicationTests.java
@@ -2,8 +2,10 @@ package com.medicareplus;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test") // trebuie adaugat la fiecare clasa de test
 class ApplicationTests {
 
 	@Test
