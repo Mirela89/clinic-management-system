09471561abd32b828fb303b369be5459230fc0af
Mirela89 <ddrt91@hotmail.com>
2026-04-23 20:13:43 +0300

    add exceptions

---
+++ backend/src/main/java/com/medicareplus/common/exception/BusinessException.java
@@ -0,0 +1,9 @@
+package com.medicareplus.common.exception;
+
+public class BusinessException extends RuntimeException {
+
+    public BusinessException(String message) {
+        super(message);
+    }
+
+}
---
+++ backend/src/main/java/com/medicareplus/common/exception/ResourceNotFoundException.java
@@ -0,0 +1,13 @@
+package com.medicareplus.common.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+
+    public ResourceNotFoundException(String resourceName, Long id) {
+        super(resourceName + " with id " + id + " not found.");
+    }
+
+}
